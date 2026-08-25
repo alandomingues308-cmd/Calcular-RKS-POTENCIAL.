3ad6549f771c13ea090970388aa045d783231507
alandomingues308-cmd <alandomingues308@gmail.com>
2026-08-24 22:51:42 -0600

    Refactor app.py for clarity and structure

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ if modo == "Arcaea":
 
 if modo == "Osu":
     R1= ((st.number_input("PP: "))**0.5)*2.8
-    R2= (st.number_input("MSD: "))**1.4)
+    R2= (st.number_input("MSD: "))**1.4
     R3= (st.number_input("DanRC: "))*12
     R4= (st.number_inout("DanLN: "))*10
     SPI= R1 + R2 + R3 + R4
